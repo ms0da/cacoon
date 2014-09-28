7deafce89ace380bb799eea822a1d6ae0b80766f
ms0da <ms0da@github.com>
2014-09-28 13:27:37 -0400

    chore(agent): adds start/stop base

--- cacoon_test/agent_test.cpp
+++ cacoon_test/agent_test.cpp
@@ -8,17 +8,40 @@ using cacoon::agent_id::value_type;
 using cacoon::comms_file;
 using std::move;
 
+using agent_file_type = agent<comms_file>;
+
 SCENARIO("give and retrieve the id of an agent", "[agent]") {  
-    using agent_type = agent<comms_file>;
-    agent_type agent;
+    agent_file_type agent;
     REQUIRE(cacoon::agent_id::DEFAULT_ID == agent.get_id());
 
     GIVEN("a value is given to the id") {
-        const agent_type::id_type new_id = 42;
+        const agent_file_type::id_type new_id = 42;
         agent.set_id(new_id);
 
         WHEN("the new value of id can be retrieved and should match") {
             REQUIRE(new_id == agent.get_id());
+        }
+    }
+}
+
+SCENARIO("start and stop", "[agent]") {
+    agent_file_type agent;
+
+    GIVEN("a new agent") {
+        THEN("the agent should not be running") {
+            REQUIRE(!agent.is_running());    
+        }
+    }
+    GIVEN("the agent starts") {
+        agent.start();
+        THEN("the agent should be running") {
+            REQUIRE(agent.is_running());
+            AND_WHEN("the agent is stopped") {
+                agent.try_stop();
+                THEN("the agent is not running anymore") {
+                    REQUIRE(!agent.is_running());
+                }
+            }
         }
     }
 }
