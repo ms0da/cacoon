f27c8c96271c6e0c675458fe8b570c1deee25954
ms0da <ms0da@github.com>
2014-09-28 13:27:37 -0400

    chore(agent): adds start/stop base

--- cacoon/agent.h
+++ cacoon/agent.h
@@ -6,6 +6,9 @@
 #include "datastore.h"
 #include <limits>
 #include <string>
+#include <atomic>
+#include <thread>
+#include <future>
 
 namespace cacoon {
 
@@ -18,11 +21,12 @@ namespace cacoon {
 
     template<typename T>
     struct agent {
-        using value_type = T;
+        using transport_type = T;
         using id_type = agent_id::value_type;
 
         agent(id_type id = agent_id::DEFAULT_ID)
-        :m_comms(T(id)), m_id(id), m_store(STORE_LOCATION) {
+        :m_comms(transport_type(id)), m_id(id), m_store(STORE_LOCATION) {
+            m_run.clear();
         }
 
         void set_id(id_type id) {
@@ -33,12 +37,47 @@ namespace cacoon {
             return m_id;
         }
 
+        void start() {
+            //m_thread = std::move(std::thread(&main_loop, this));
+            if(!m_run.test_and_set()) {
+
+            }
+        }
+
+        bool is_running() {
+            const bool is_running = m_run.test_and_set();
+            if(!is_running) {
+                m_run.clear();
+            }
+            return is_running;
+        }
+
+        void try_stop() {
+            m_run.clear();
+            //if(m_thread.joinable()) {
+            //    m_thread.join();
+            //}
+        }
+
     private:
-        
+
+        void main_loop() {
+            
+        }
+
+        void loop_step() {
+            std::cout << "Running0" << std::endl;
+            Sleep(5000);
+            std::cout << "Running1" << std::endl;
+        }
 
         id_type m_id;
         comms m_comms;
         datastore m_store;
+
+        std::atomic_flag m_run;
+        //std::thread m_thread;
+        //auto m_future;
     };
 }
 
